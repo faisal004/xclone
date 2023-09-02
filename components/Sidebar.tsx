104edab2a83077478baa943e570f14db61d28391
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-09-02 17:11:35 +0530

    authentication added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,38 +1,86 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import { FaTwitter } from 'react-icons/fa'
 import { AiFillHome, AiFillBell } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
-import { BiLogOut } from 'react-icons/bi'
+import { BiLogOut, BiLogInCircle } from 'react-icons/bi'
+import { signOut, useSession, signIn } from 'next-auth/react'
 import Link from 'next/link'
+import Loginmodal from '@/modals/Loginmodal'
+
+
 interface MenuItemProps {
   icon: ReactNode
   text: string
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ icon, text }) => (
-  <div className="flex flex-row p-2 items-center space-x-1 cursor-pointer hover:bg-slate-900 rounded-xl  ">
+  <div className="flex flex-row p-2 items-center space-x-1 cursor-pointer hover:bg-slate-900 rounded-xl">
     <div>{icon}</div>
     <div className="text-center hidden md:block">{text}</div>
   </div>
 )
 
 const Sidebar: React.FC = () => {
+  const { data: session } = useSession()
+  console.log(session)
+
+  const handleLogout = async () => {
+    await signOut()
+  }
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
-    <div className="text-white w-1/4 hidden md:block h-screen sticky top-0 ">
-      <div className="flex justify-start p-3 ">
-        <FaTwitter />
+    <div className="text-white w-1/4 hidden md:block h-screen sticky top-0">
+      <div className="flex justify-start p-3">
+        <Link href="/">
+          {' '}
+          <FaTwitter />
+        </Link>
       </div>
-      <div className="flex flex-col mx-auto  ">
-        <MenuItem icon={<AiFillHome />} text="Home" />
-        
+      <div className="flex flex-col mx-auto">
+        <Link href="/">
+          {' '}
+          <MenuItem icon={<AiFillHome />} text="Home" />{' '}
+        </Link>
+
         <MenuItem icon={<AiFillBell />} text="Notifications" />
-        <Link href={"/Profile"}><MenuItem icon={<CgProfile />} text="Profile" /> </Link>
-        
-        <MenuItem icon={<BiLogOut />} text="Logout" />
+        <Link href="/Profile">
+          <MenuItem icon={<CgProfile />} text="Profile" />{' '}
+        </Link>
+        {session ? (
+          <div
+            className="flex flex-row p-2 items-center space-x-1 cursor-pointer hover:bg-slate-900 rounded-xl"
+            onClick={handleLogout}
+          >
+            <div>
+              <BiLogOut />
+            </div>
+            <div className="text-center hidden md:block">Logout</div>
+          </div>
+        ) : (
+          <div
+            className="flex flex-row p-2 items-center space-x-1 cursor-pointer hover:bg-slate-900 rounded-xl"
+            onClick={openModal}
+          >
+            <div>
+              <BiLogInCircle />
+            </div>
+            <div className="text-center hidden md:block">Login</div>
+          </div>
+        )}
       </div>
-      <div className="w-1/3 pt-2 flex flex-row p-2 items-start justify-start ">
+      <div className="w-1/3 pt-2 flex flex-row p-2 items-start justify-start">
         <button className="bg-[#2f6eb9] w-full rounded-2xl p-1">Tweet</button>
       </div>
+      {isModalOpen && <Loginmodal closeModal={closeModal} />}
     </div>
   )
 }
