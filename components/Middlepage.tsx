55be568865f568d34fc4fecaa55c24fd146e634d
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-09-02 17:11:47 +0530

    authentication added

--- components/Middlepage.tsx
+++ components/Middlepage.tsx
@@ -4,14 +4,18 @@ import Tweetbox from '@/pagecomponents/Tweetbox'
 import Notloginbox from '@/pagecomponents/Notloginbox';
 import { useRecoilState } from "recoil";
 import { userIn } from '@/store/atoms/userIn';
+import {  useSession } from 'next-auth/react'
+
+
 
 const Middlepage = () => {
   const [user, ] = useRecoilState(userIn);
+  const { status } = useSession()
  
   return (
     <div className='text-white border-l border-r border-l-slate-50 border-r-slate-50  md:w-2/4 w-full  items-center  '>
         <div className='bg-black border-b-2 text-left p-2'>Home</div>
-        <div>{user?<Tweetbox/>:
+        <div>{status==="authenticated"?<Tweetbox/>:
         <Notloginbox/>}</div>
         
     </div>
--- pagecomponents/Tweetbox.tsx
+++ pagecomponents/Tweetbox.tsx
@@ -1,13 +1,22 @@
 import React, { FC } from 'react'
+import  {useSession} from "next-auth/react"
+import Image from 'next/image'
 
 const Tweetbox: FC = () => {
+  const {data:session}=useSession()
+  console.log(session?.user?.image)
   return (
     <div className="flex flex-row justify-between border-b-2 pb-2">
-      <div className="profile p-2 m-2">Profile</div>
+      <div className='rounded-full p-2 m-2 '>
+      <Image className='rounded-full' src={session?.user?.image} alt="sd" height={50} width={50}></Image>
+
+
+      </div>
+      
       <div>
         <div className="textarea bg-black text-white p-2 m-2">
           <textarea
-            className="resize-none bg-black outline-none"
+            className="resize-none bg-black outline-none w-11/12"
             name="name"
             cols={70}
             rows={5}
